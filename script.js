9f82e15f90597905733a2259d67608bf1d3b058c
Brady <brady.savarie@hotmail.com>
2023-01-20 23:56:55 -0500

    Validate and style form

---
+++ script.js
@@ -0,0 +1,21 @@
+const password = document.getElementById('password');
+const confirm = document.getElementById('confirmpassword');
+
+password.addEventListener('keyup', e => {
+    isValid = password.checkValidity();
+    if (isValid) {
+        password.className = 'success';
+    }
+    else {
+        password.className = 'error';
+    }
+});
+
+confirm.addEventListener('keyup', e => {
+    if (confirm.value === password.value) {
+        confirm.className = 'success';
+    }
+    else {
+        confirm.className = 'error';
+    }
+})
