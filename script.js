8e8c5e71ed31ebfe1634105d8eb015eba4412bbf
Brady <brady.savarie@hotmail.com>
2023-01-29 01:26:29 -0500

    Enable eslint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const password = document.getElementById('password');
 const confirm = document.getElementById('confirmpassword');
 
-password.addEventListener('keyup', (e) => {
-    isValid = password.checkValidity();
+password.addEventListener('keyup', () => {
+    const isValid = password.checkValidity();
     if (isValid) {
         password.className = 'success';
     } else {
@@ -10,7 +10,7 @@ password.addEventListener('keyup', (e) => {
     }
 });
 
-confirm.addEventListener('keyup', (e) => {
+confirm.addEventListener('keyup', () => {
     if (confirm.value === password.value) {
         confirm.className = 'success';
     } else {
